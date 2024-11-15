81fb4e2227070c8d05610a0d57756c6728c5c7e1
mfadlyx <mfadly1010@gmail.com>
2024-11-15 21:27:54 +0700

    Add Laporan Semua page and Laporan Semua test

--- src/main/java/com/juaracoding/utils/ScenarioTests.java
+++ src/main/java/com/juaracoding/utils/ScenarioTests.java
@@ -15,7 +15,17 @@ public enum ScenarioTests {
     T11("Password masking verification when entering password"),
     T12("Disable password masking"),
     //dashboard
-    T13("Successfully go to the dashboard page");
+    T13("Successfully go to the dashboard page"),
+    //laporanSemua
+    T14("Success view all report data by entering name, department, date"),
+    T15("View all report data by not enter name, department, date"),
+    T16("View report data by using Input unregistered name"),
+    T17("View all report data by entering the start date and end date"),
+    T18("Display all report data by entering a start date without an end date"),
+    T19("Display all report data by entering end date without start date"),
+    T20("Displays report data all with department and date input"),
+    T21("Entering dates is not formatted"),
+    T22("Reset all inputs using the Reset button");
 
     private String scenarioTestName;
 
