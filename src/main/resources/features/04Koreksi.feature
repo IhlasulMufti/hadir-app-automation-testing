63221d52af24888b1a7e485abaec0c989859498a
giffari <giffarij@gmail.com>
2025-02-06 13:29:58 +0700

    feature sakit dan lembur

--- src/main/resources/features/04Koreksi.feature
+++ src/main/resources/features/04Koreksi.feature
@@ -1,6 +1,42 @@
-Feature: All data
+Feature: Koreksi
 
-  Scenario: Menampilkan data laporan semua dengan menginput nama, departemen,tanggal
+  Scenario Outline: Menampilkan data koreksi dengan input nama dan tanggal yang sesuai
     Given Klik button menu laporan
-    When Klik button menu semua
-    And Masukkan 
+    When Klik button menu koreksi
+#    And Masukkan nama yang sesuai "<nama>"
+#    And Klik button date
+#    And Pilih start date yang sesuai "<startdate>"
+#    And Pilih end date yang sesuai "<enddate>"
+#    And klik button save date
+#    And Klik button search
+#    Then Verifikasi terdapat data dengan nama dan tanggal yang sesuai
+#    And Klik button reset
+
+    Examples:
+       | nama     | startdate     | enddate |
+       |          | Nov 07, 2024  | Nov 09, 2024 |
+       |UserQA    | Nov 07, 2024  | Nov 09, 2024 |
+
+  Scenario: Approval koreksi absen yang diajukan
+    Given Masukkan nama yang valid
+    When Klik button search
+    And Klik icon approval
+    And Klik button setujui
+    Then Verifikasi status karyawan
+    And Klik button reset
+
+  Scenario Outline: Reject koreksi absen yang diajukan
+    Given Masukkan nama yang valid "<nama>"
+    When  Klik button date
+    And Masukkan start date valid "<startdate>"
+    And Masukkan end date valid "<enddate>"
+    And klik button save date
+    And Klik button search
+    And Klik button reject
+    And Masukkan Alasan reject
+    And Klik button tolak
+    Then Verifikasi Status karyawan
+
+    Examples:
+      | nama        | startdate     | enddate      |
+      | Test User 1 | Nov 08, 2024  | Nov 08, 2024 |
--- src/test/java/com/juaracoding/admin/KoreksiTest.java
+++ src/test/java/com/juaracoding/admin/KoreksiTest.java
@@ -1,5 +1,91 @@
 package com.juaracoding.admin;
 
-public class SemuaTest {
+import com.juaracoding.Hooks;
+import com.juaracoding.pages.admin.KoreksiPage;
+import com.juaracoding.pages.admin.LemburPage;
+import com.juaracoding.pages.admin.SakitPage;
+import com.juaracoding.utils.Utils;
+import com.relevantcodes.extentreports.ExtentTest;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.zh_cn.假如;
+import jdk.jshell.execution.Util;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+
+public class KoreksiTest {
+    private static WebDriver driver;
+    private ExtentTest extentTest;
+   private static KoreksiPage koreksiPage = new KoreksiPage();
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    ArrayList<String> tab;
+
+
+
+
+    public KoreksiTest(){
+        driver = Hooks.driver;
+        extentTest = Hooks.extentTest;
+    }
+
+    @When("Klik button menu koreksi")
+    public void klik_button_menu_koreksi(){
+        koreksiPage.setSubMenuKoreksi();
+        Utils.delay(2);
+    }
+
+    @And("Masukkan nama yang sesuai {string}")
+    public void masukkan_nama_yang_sesuai(String nama){
+        koreksiPage.setInputSearch(nama);
+    }
+
+    @And("Pilih start date yang sesuai {string}")
+    public void pilih_start_date_yang_sesuai(String startdate){
+        koreksiPage.setInputStartDate(startdate);
+        Utils.delay(2);
+    }
+
+    @And("Pilih end date yang sesuai {string}")
+    public void pilih_end_date_yang_sesuai(String enddate){
+        koreksiPage.setInputEndDate(enddate);
+    }
+
+    @Then("Verifikasi terdapat data dengan nama dan tanggal yang sesuai")
+    public void verifikasi_terdapat_data_dengan_nama_dan_tanggal_yang_sesuai(){
+        Assert.assertEquals(koreksiPage.getTxt08Nov(),"08 Nov 2024");
+        Assert.assertEquals(koreksiPage.getTxtUserQA(),"UserQA");
+    }
+
+    @Given("Masukkan nama yang valid")
+    public void masukkan_nama_yang_valid(){
+        koreksiPage.setInputSearch("testerjuara");
+        js.executeScript("window.scrollBy(-700,0)");
+    }
+
+    @And("Klik icon approval")
+    public void klik_icon_approval(){
+        Utils.delay(2);
+        koreksiPage.setBtnApproval();
+    }
+
+    @And("Klik button setujui")
+    public void klik_button_setujui(){
+        koreksiPage.setBtnSetuju();
+    }
+
+    @Then("Verifikasi status karyawan")
+    public void verifikasi_status_karyawan(){
+        koreksiPage.getTxtApproved();
+    }
+
+
+
+
 
 }
+
